016253005e8c4c317309eae0e3c5b454e0a94cd1
Abhinav <abhiasthana76@gmail.com>
2024-05-31 14:33:02 +0530

    well yep this is boring

---
+++ python/python_intro/9_if_else/if.py
@@ -0,0 +1,15 @@
+# This is a demonstration of  if command
+
+a, b = input("Enter the numbers: ").split()
+
+print("Enterd NUmbers: ", a, b)
+
+if a == b:
+    print("a equals b")
+elif a < b:
+    print("a is smaller")
+else:
+    print("a is bigger")
+
+
+
