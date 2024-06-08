50e8916281573cd563554f5d4a3165e2c6148c0a
Abhinav <abhiasthana76@gmail.com>
2024-06-08 22:09:42 +0530

    Reversing each string

---
+++ python/python_intro/12_list_comprehension/rev_str.py
@@ -0,0 +1,8 @@
+# -*- coding=utf-8 -*-
+
+# this is a program to reverse each string in a list
+strn = ['apple', 'banana', 'coco', 'melon', 'watermelon']
+
+reverse = [strn[::-1] for strn in strn]
+print(reverse)
+
