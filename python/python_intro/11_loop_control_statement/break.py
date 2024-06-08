77412ccbdcdf3e42ad6e1b3cbfdf35ea27cb030a
Abhinav <abhiasthana76@gmail.com>
2024-06-08 11:23:44 +0530

    Loop control statements

---
+++ python/python_intro/11_loop_control_statement/break.py
@@ -0,0 +1,12 @@
+str1 = "This is a loop control statement demonstration"
+
+
+for i in str1:
+    if i == 'l':
+        print("breaking loop at l character.")
+        break
+    print(i, end=' ')
+
+
+
+
