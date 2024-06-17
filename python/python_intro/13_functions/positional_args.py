480cff29a888fb218b15cccb86553cdf912e844b
Abhinav <abhiasthana76@gmail.com>
2024-06-17 09:48:07 +0530

    Postional arguments

---
+++ python/python_intro/13_functions/positional_args.py
@@ -0,0 +1,12 @@
+# program about positional arguments in python
+
+def posFunc(name, age):
+    print(f"Hii my name is {name}.")
+    print(f"My age is {age}.")
+
+posFunc('abhi', 19)
+posFunc(19, 'abhi')
+
+
+
+
