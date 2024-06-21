dfa2ede76907c5dd2bedb02e055c0eb4141289a8
Abhinav <abhiasthana76@gmail.com>
2024-06-21 10:58:56 +0530

    Writing to a file

---
+++ python/python_intro/14_file_handling.py/writing_file.py
@@ -0,0 +1,10 @@
+# Open a file with write permission
+file = open('example.txt', 'w')
+
+# write some text to the file
+file.write("Hello, world!.\nThis is a test file.")
+
+#close the file
+file.close()
+
+
