7b3f8a5b5ffdbf47cc56d08a6f5e0d7d83136cb5
Abhinav <abhiasthana76@gmail.com>
2024-06-21 11:23:04 +0530

    appending to a file

---
+++ python/python_intro/14_file_handling.py/appending_file.py
@@ -0,0 +1,9 @@
+# a program about appending to a string
+
+# open the file in append mode
+
+file = open('example.txt', 'a')
+
+# append some text to a file
+file.write("\n This line is appended")
+file.close()
