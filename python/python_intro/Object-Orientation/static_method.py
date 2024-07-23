9e4bea16bac60a7056db7afcf0cfe3c37edfa1f2
Abhinav <abhiasthana76@gmail.com>
2024-07-23 11:22:04 +0530

    Static method in oops

--- python/python_intro/Object-Orientation/static_method.py
+++ python/python_intro/Object-Orientation/static_method.py
@@ -7,12 +7,20 @@ class Math:
 
     @staticmethod
     def add(a, b):
+        """
+        Static method in python are  methods that belongs to 
+        the class rather than an instance of the class.
+        They are called on the basis of the class not on an 
+        instance of a class
+        """
+        print(Math.add.__doc__)
         return a+b
 a = Math(5)
 print(a.num)
 
 a.addtonum(6)
 print(a.num)
+# print(Math.add.__doc__)
 
 # using class
 print(f"Static method: {Math.add(7, 2)}")
